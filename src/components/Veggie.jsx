23a3ba8eee5d67ba114caabe11060b1065dfc33c
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2022-03-16 10:09:49 +0500

    Tayyor

---
+++ src/components/Veggie.jsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import { Link } from "react-router-dom";
+
+function Veggie() {
+  const [veggie, setVeggie] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const check = localStorage.getItem("veggie");
+
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=9f363848a00b4f8f8b2179ea55fe5207&number=9&tags=vegetarian`
+      );
+      const data = await api.json();
+      localStorage.setItem("veggie", JSON.stringify(data.recipes));
+      setVeggie(data.recipes);
+    }
+  };
+
+  return (
+    <div>
+      <Wrapper>
+        <h3>Our Vegetarian Picks</h3>
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {veggie.map((rec) => {
+            return (
+              <SplideSlide key={rec.id}>
+                <Card>
+                  <Link to={`/recipe/` + rec.id}>
+                    <p>{rec.title}</p>
+                    <img src={rec.image} alt={rec.title} />
+                    <Gradient />
+                  </Link>
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
+}
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+const Card = styled.div`
+  min-height: 23rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 0;
+    bottom: 0%;
+    transform: translateX(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1.2rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
+
+export default Veggie;
---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { motion } from "framer-motion";
+
+function Recipe() {
+  const [details, setDetails] = useState([]);
+  const [activeTab, setActiveTab] = useState("instructions");
+  let params = useParams();
+  console.log(details);
+  console.log(params.name);
+
+  const fetchDetails = async () => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=9f363848a00b4f8f8b2179ea55fe5207`
+    );
+    const detailData = await data.json();
+    setDetails(detailData);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+    // eslint-disable-next-line
+  }, [params.name]);
+
+  return (
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt={details.title} />
+      </div>
+      <Info>
+        <Button
+          className={activeTab === "instructions" ? "active" : ""}
+          onClick={() => setActiveTab("instructions")}
+        >
+          Instructions
+        </Button>
+        <Button
+          className={activeTab === "ingredients" ? "active" : ""}
+          onClick={() => setActiveTab("ingredients")}
+        >
+          Ingredients
+        </Button>
+        {activeTab === "instructions" && (
+          <motion.div
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+            <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+          </motion.div>
+        )}
+        {activeTab === "ingredients" && (
+          <motion.div
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <ul>
+              {details.extendedIngredients.map((ingredient) => {
+                return <li key={ingredient.id}>{ingredient.original}</li>;
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </Info>
+    </DetailWrapper>
+  );
+}
+
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  h2 {
+    margin-bottom: 2rem;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.div`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+  display: inline;
+`;
+
+const Info = styled.div`
+  margin-left: 10rem;
+`;
+export default Recipe;
